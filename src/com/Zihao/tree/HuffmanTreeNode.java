e51091cb854aab7269dc6c72cd0c7d3dd10e9d98
ZihaoHan <972802149@qq.com>
2021-03-14 22:04:59 -0700

    add huffman code

--- src/com/Zihao/tree/HuffmanTreeNode.java
+++ src/com/Zihao/tree/HuffmanTreeNode.java
@@ -1,8 +1,16 @@
 package com.Zihao.tree;
 
 public class HuffmanTreeNode<T> extends TreeNode<T> implements Comparable<HuffmanTreeNode> {
+    private int weight;
+
     public HuffmanTreeNode(T value) {
         super(value);
+        this.weight = (Integer) value;
+    }
+
+    public HuffmanTreeNode(T value, int weight) {
+        super(value);
+        this.weight = weight;
     }
 
     public void setLeft(HuffmanTreeNode left) {
@@ -13,13 +21,21 @@ public class HuffmanTreeNode<T> extends TreeNode<T> implements Comparable<Huffma
         this.right = right;
     }
 
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
     @Override
     public int compareTo(HuffmanTreeNode o) {
-        return (Integer) this.getValue() - (Integer)o.getValue();
+        return this.weight - o.getWeight();
     }
 
     @Override
     public String toString() {
-        return ""+getValue();
+        return ""+getWeight();
     }
 }
